664b4ecee0b7f7acfaa10eddf687f20c35c211e4
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-05-08 11:17:52 +0100

    Initial Commit

---
+++ SDL Pong/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDL Pong
+//
+//  Created by Alex Donisthorpe on 08/05/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
